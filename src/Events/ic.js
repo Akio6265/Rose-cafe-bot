3f95c44ded3f0f8cbad0a219fa244df83c967f06
Aki <96163662+Akio6265@users.noreply.github.com>
2023-08-07 19:06:51 +0530

    mongodb

--- src/Events/ic.js
+++ src/Events/ic.js
@@ -1,22 +1,47 @@
-const { Events } = require('discord.js');
+const { Events, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     name: Events.InteractionCreate,
+    /**
+     * 
+     * @param {import('discord.js').Interaction} interaction  
+     */
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.slash_cmd.get(interaction.commandName);
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
 
-        const command = interaction.client.slash_cmd.get(interaction.commandName);
-
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
         }
+        else if (interaction.isButton()) {
+            const channel = interaction.channel;
+            const user = interaction.user;
+            if (interaction.customId === 'claim') {
+                // Update channel permissions
+                // await channel.permissionOverwrites.edit('1088855389824618517', {
+                //     ViewChannel: false
+                // });
+                await channel.permissionOverwrites.edit(user.id, {
+                    ViewChannel: true
+                });
+                // Send a response message
+                interaction.reply({
+                    content: `This ticket is claimed by <@${user.id}>`
+                });
+            }
+            if (interaction.customId === 'close' && interaction.id !== user.id) {
+                await channel.delete();
+                user.send('Your ticket has been closed');
+            }
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
         }
-    },
+    }
 };
--- src/reg_cmds/dm_cmds/modmail.js
+++ src/reg_cmds/dm_cmds/modmail.js
@@ -1,7 +1,7 @@
-const { EmbedBuilder, Message, ChannelType, userMention, roleMention, time } = require('discord.js')
+const { EmbedBuilder, Message, ChannelType, userMention, roleMention, time, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js')
 const { gId, categoryId } = require('../../../config.json');
 const rose = require('../../bot');
-const moment = require('moment');
+
 
 module.exports = {
     name: "create",
@@ -17,33 +17,48 @@ module.exports = {
             const category = guild.channels.cache.get(categoryId)
             // Check if the user has an existing ticket
             const existingChannel = guild.channels.cache.find(channel => channel.name === msg.author.tag);
-            // if (existingChannel) {
-            //     return msg.channel.send(`❌ There is already a ticket opened by you!`);
-            // };
+            if (existingChannel) {
+                return msg.channel.send(`❌ There is already a ticket opened by you!`)
+                    .then(e => msg.react('❌'));
+
+            };
             let member = guild.members.cache.get(msg.author.id);
             if (!member) member = await guild.members.fetch(msg.author.id);
             let rolesMap = member.roles.cache;
             if (!rolesMap) rolesMap = await member.roles.fetch();
 
             const roles = rolesMap.map((role) => roleMention(role.id)).join(' ');
-
-
-
             const created = time(member.user.createdAt, 'R');
             const joined = time(member.joinedAt, 'R');
+
+            const claim = new ButtonBuilder()
+                .setCustomId('claim')
+                .setLabel('Claim')
+                .setEmoji('🫳')
+                .setStyle(ButtonStyle.Primary);
+
+            const close = new ButtonBuilder()
+                .setCustomId('close')
+                .setLabel('Close')
+                .setStyle(ButtonStyle.Danger)
+                .setEmoji('🔒');
+
+
+            const comp = new ActionRowBuilder()
+                .addComponents(claim, close);
+
             const mention = new EmbedBuilder()
                 .setColor(0xe645b8)
                 .setAuthor({
                     name: msg.author.username,
                     iconURL: msg.author.displayAvatarURL({ dynamic: true })
                 })
-                .setTitle(userMention(msg.author.id))
+                .setDescription(userMention(msg.author.id), ` Created ${created} and joined ${joined}`)
                 .addFields({
                     name: "Roles",
                     value: roles,
                     inline: false
                 })
-                .setDescription(`Created ${created} and joined ${joined}`)
                 .setFooter({ text: `UserID: ${msg.author.id}` })
                 .setTimestamp();
 
@@ -53,13 +68,29 @@ module.exports = {
                 type: ChannelType.GuildText,
                 description: msg.author.id,
                 parent: category,
-                topic: `Ticket for ${msg.author.username}`
-            });
+                topic: `Ticket for ${msg.author.id}`,
+                permissionOverwrites: [
+                    {
+                        id: guild.id,
+                        deny: 'ViewChannel'
+                    },
+                    {
+                        id: msg.author.id,
+                        allow: ['ViewChannel']
+                    },
+                    {
+                        id: '1088855389824618517',//staff role here
+                        allow: ['ViewChannel']
+                    }
 
-            channel.send({ embeds: [mention] })
+                ]
+            });
+            channel.lockPermissions()
+                .catch(e => channel.send('some error appeared'))
+            channel.send({ embeds: [mention], components: [comp] })
                 .then(e => {
                     msg.react('✅');
-                    msg.reply('Your ticket has been created, Please wait for someone to respond.');
+                    msg.channel.send(`Your ticket has been created <#${channel.id}> , Please wait for someone to respond.`);
                 });
         } catch (err) {
             console.log(err)
