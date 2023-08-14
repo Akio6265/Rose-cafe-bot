28f0d6f0d54a8daed2fe32c985b8e130e026f746
Aki <96163662+Akio6265@users.noreply.github.com>
2023-08-14 18:03:19 +0530

    nom

--- src/Events/join.js
+++ src/Events/join.js
@@ -35,7 +35,7 @@ module.exports = {
             .setColor(0xfab4e3)
             .setThumbnail('https://cdn.discordapp.com/attachments/1139856341381431376/1139981548955910324/image0.gif')
             .setAuthor({ name: `Welcome ${member.user.username} in E-Girl social!`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
-            .setDescription(':EGSCLOUD_: **Come chat with us in** https://discord.com/channels/1029523457240739860/1123552387760672778\n\n:EGS_CLOUD: [Come join us with VC!](https://discord.com/channels/1029523457240739860/1056072538674970694)\n\n:EGSCLOUD: **feel free to check out our giveaways **https://discord.com/channels/1029523457240739860/1133162151662604438 https://discord.com/channels/1029523457240739860/1129470784654557294 https://discord.com/channels/1029523457240739860/1139692436323565668')
+            .setDescription('<a:EGS_CLOUD:1124928678535630849>  **Come chat with us in** https://discord.com/channels/1029523457240739860/1123552387760672778\n\n<a:EGSCLOUD:1124928621694427156>  [Come join us with VC!](https://discord.com/channels/1029523457240739860/1056072538674970694)\n\n<a:EGSCLOUD:1124928621694427156>  **feel free to check out our giveaways **https://discord.com/channels/1029523457240739860/1133162151662604438 https://discord.com/channels/1029523457240739860/1129470784654557294 https://discord.com/channels/1029523457240739860/1139692436323565668')
             .setImage('https://cdn.discordapp.com/attachments/1139856341381431376/1139985585411473408/divider1.gif')
             .setFooter({ text: 'E-Girl social wishes you a pleasent stay' })
         channel.send({ content: `:1499sparkleheart: welcome ${userMention(member.user.id)} :1499sparkleheart:`, embeds: [welcome], components: [row] });
