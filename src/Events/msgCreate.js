2d7dfef01a97e1cb50074236d13ac8374db40fef
Aki <96163662+Akio6265@users.noreply.github.com>
2023-08-21 20:35:46 +0530

    :D

--- src/Events/msgCreate.js
+++ src/Events/msgCreate.js
@@ -67,7 +67,7 @@ module.exports = {
             const args = message.content.slice(1).trim().split(/ +/);
             const commandName = args.shift().toLowerCase();
             const command = rose.reg_cmd.get(commandName)
-            if (!command) return message.channel.send("use ?create to create a ticket");
+            if (!command) return message.channel.send("use **?create** to create a ticket");
             try {
                 command.execute(message, args);
             } catch (error) {
@@ -82,15 +82,7 @@ module.exports = {
             const commandName = args.shift().toLowerCase();
 
             const command = rose.reg_cmd.get(commandName)
-            if (!command) return message.react('<a:OHWOOOW:1038546514907250818>').then(() => {
-                message.reply('I dont have this command...')
-                    .then((msg) => {
-                        setTimeout(() => {
-                            msg.delete();
-                        }, 4000);
-
-                    });
-            }).catch(err => console.log(err));
+            if (!command) return;
             try {
                 command.execute(message, args);
             } catch (error) {
